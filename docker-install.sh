97ad557d571c2c32a7a7215eaafab44eb0210621
HShanks19 <78905676+HShanks19@users.noreply.github.com>
2021-03-24 17:00:33 +0000

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -5,6 +5,15 @@ curl https://get.docker.com | sudo bash
 
 sudo usermod -aG docker jenkins
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-\$(uname -s)-\$(uname -m) -o /usr/local/bin/docker-compose
+# make sure jq & curl is installed
+sudo apt update
+sudo apt install -y curl jq
+# set which version to download (latest)
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+# download to /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# make the file executable
 sudo chmod +x /usr/local/bin/docker-compose
+
+#login to docker
 echo $DOCKER_PASSWORD | docker login --username hollyshanks --password-stdin
