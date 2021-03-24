3581af0104ecede2217599380919e6df5a7415af
HShanks19 <78905676+HShanks19@users.noreply.github.com>
2021-03-24 16:55:33 +0000

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 curl https://get.docker.com | sudo bash
 
 sudo usermod -aG docker jenkins
-sudo systemctl restart jenkins
+
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-\$(uname -s)-\$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
